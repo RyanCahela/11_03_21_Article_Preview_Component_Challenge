c8b1f563a19ada121ce4f8661698bab3c3c9cb28
Ryan Cahela <6194542+RyanCahela@users.noreply.github.com>
2021-11-04 16:11:01 -0700

    refactored share widget, almost there

--- src/main.js
+++ src/main.js
@@ -1,23 +1,17 @@
 //get buttons
-const shareWidgetToggleOn = document.querySelector(
-  ".js-card__share-icon--toggle-on"
-);
-
-const shareWidgetToggleOff = document.querySelector(
-  ".js-card__share-icon--toggle-off"
-);
+const shareIcon = document.querySelector(".js-card__share-icon");
 
 const shareWidget = document.querySelector(".js-card__share-widget");
 
 //set click listener on buttons
-shareWidgetToggleOn.addEventListener("click", (e) => {
+shareIcon.addEventListener("click", (e) => {
   //toggle on removes class
-  console.log("toggleOn clicked");
-  shareWidget.classList.remove("hidden");
+  console.log("sharIcon clicked");
+  shareWidget.classList.toggle("transparent");
 });
 
 //toggle off adds class
 shareWidgetToggleOff.addEventListener("click", (e) => {
   console.log("toggleOff clicked");
-  shareWidget.classList.add("hidden");
+  shareWidget.classList.add("transparent");
 });
