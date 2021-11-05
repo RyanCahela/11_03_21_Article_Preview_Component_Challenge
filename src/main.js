6d2fabb4e8e4e9024d4c2547369741de2d18d126
Ryan Cahela <6194542+RyanCahela@users.noreply.github.com>
2021-11-05 10:12:39 -0700

    refactored, created media query to handle share widget on screens 350px or smaller

--- src/main.js
+++ src/main.js
@@ -1,17 +1,11 @@
 //get buttons
 const shareIcon = document.querySelector(".js-card__share-icon");
 
+//get component
 const shareWidget = document.querySelector(".js-card__share-widget");
 
 //set click listener on buttons
 shareIcon.addEventListener("click", (e) => {
   //toggle on removes class
-  console.log("sharIcon clicked");
   shareWidget.classList.toggle("transparent");
-});
-
-//toggle off adds class
-shareWidgetToggleOff.addEventListener("click", (e) => {
-  console.log("toggleOff clicked");
-  shareWidget.classList.add("transparent");
 });
